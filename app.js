c31581ac25afe64e6c192ed9190a7934529e37a5
ArthurKanzaki <arthurvieirapedrosa@hotmail.com>
2023-01-18 17:15:41 -0300

    retornando com as funções de testes no app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Rodando o sistema de gerenciamento de jogos.");
+
+function teste() {
+    console.log("testando evento");
+}
+
+function teste2() {
+    console.log("testando evento 2");
+}
+
+function teste2() {
+    console.log("testando evento 3");
+}
