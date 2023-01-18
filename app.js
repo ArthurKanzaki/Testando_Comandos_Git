c5b5fea3555387d77477b1adf6458e5c31f09d98
Arthur Vieira <arthurvieirapedrosa@hotmail.com>
2023-01-18 16:29:06 -0300

    criando função de evento 2 no app.js

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log("Rodando o sistema de gerenciamento de jogos.");
 function teste() {
     console.log("testando evento");
 }
+
+function teste2() {
+    console.log("testando evento 2");
+}
